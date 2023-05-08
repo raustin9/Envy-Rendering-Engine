af3f9c6ad48713125a3aa22bc1866a12e769469d
Reagan Austin <raustin9@vols.utk.edu>
2023-05-08 00:05:31 -0400

    remove loadtexture function

--- js/RenderEngine.js
+++ js/RenderEngine.js
@@ -157,69 +157,6 @@ export default class RenderEngine {
   }
 
   /**
-   * This function loads a texture and creates a gl texture from it
-   * @param {String} src source file to load texture from
-   * @returns WebglContext texture2d
-   */
-  LoadTexture(src) {
-    const texture = this.GL.createTexture();
-    this.GL.bindTexture(this.GL.TEXTURE_2D, texture);
-  
-    const level = 0;
-    const internalFormat = this.GL.RGBA;
-    const width = 1;
-    const height = 1;
-    const border = 0;
-    const srcFormat = this.GL.RGBA;
-    const srcType = this.GL.UNSIGNED_BYTE;
-    const pixel = new Uint8Array([0, 0, 255, 255]); // opaque blue
-    this.GL.texImage2D(
-      this.GL.TEXTURE_2D,
-      level,
-      internalFormat,
-      width,
-      height,
-      border,
-      srcFormat,
-      srcType,
-      pixel
-    );
-  
-    const image = new Image();
-    image.onload = () => {
-      this.GL.bindTexture(this.GL.TEXTURE_2D, texture);
-      this.GL.texImage2D(
-        this.GL.TEXTURE_2D,
-        level,
-        internalFormat,
-        srcFormat,
-        srcType,
-        image
-      );
-  
-      if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-        // Yes, it's a power of 2. Generate mips.
-        this.GL.generateMipmap(this.GL.TEXTURE_2D);
-      } else {
-        // No, it's not a power of 2. Turn off mips and set
-        // wrapping to clamp to edge
-        this.GL.texParameteri(this.GL.TEXTURE_2D, this.GL.TEXTURE_WRAP_S, this.GL.CLAMP_TO_EDGE);
-        this.GL.texParameteri(this.GL.TEXTURE_2D, this.GL.TEXTURE_WRAP_T, this.GL.CLAMP_TO_EDGE);
-        this.GL.texParameteri(this.GL.TEXTURE_2D, this.GL.TEXTURE_MIN_FILTER, this.GL.LINEAR);
-      }
-    };
-    image.src = src;
-
-    this.GL.bindTexture(this.GL.TEXTURE_2D, null);
-  
-    return {
-      glTexture: texture,
-      image: image,
-    }
-    return texture;
-  }
-
-  /**
    * This function creates an instance of an object from the values in the parameter, and then adds it to 
    * the list of objects to be rendered
    * @param {JSON.Object} sourceMap map of values that are used to create an object to be rendered
